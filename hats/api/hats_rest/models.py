bfe7ac6d21845e6e6751a384a3f6adc644b3be9c
Amanda Taing <amandataing00@gmail.com>
2023-08-29 17:32:03 -0700

    Created Hat and LocationVO model, list hat view

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class LocationVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True)
+    closet_name = models.CharField(max_length=150)
+    section_number = models.PositiveSmallIntegerField()
+    shelf_number = models.PositiveSmallIntegerField()
+
+class Hat(models.Model):
+    """
+    The Hat model represents a hat that a person wants to add to their closet
+
+    """
+    style = models.CharField(max_length=150)
+    color = models.CharField(max_length=150)
+    fabric = models.CharField(max_length=150)
+    picture_url = models.URLField(null=True)
+
+    location = models.ForeignKey(
+        LocationVO,
+        related_name="hats",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
--- hats/api/hats_rest/views.py
+++ hats/api/hats_rest/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 
-# Create your views here.
+from common.json import ModelEncoder
+from .models import LocationVO, Hat
+
+class LocationVOEncoder(ModelEncoder):
+    model = LocationVO
+    properties = [
+        "import_href",
+        "closet_name",
+        "section_number",
+        "shelf_number",
+    ]
+class HatListEncoder(ModelEncoder):
+    model = Hat
+    properties = [
+        "id",
+        "style",
+        "picture_url",
+    ]
+
+class HatDetailEncoder(ModelEncoder):
+    model = Hat
+    properties = [
+        "id",
+        "style",
+        "color",
+        "fabric",
+        "picture_url",
+        "location",
+    ]
+    encoders = {"location": LocationVOEncoder()}
+
+@require_http_methods(["GET", "POST"])
+def api_list_hats(request):
+    if request.method == "GET":
+        hats = Hat.objects.all()
+        return JsonResponse(
+            {"hats": hats},
+            encoder=HatListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+
+        # try:
+        #     location_href = content["location"]
+        #     location = LocationVO.objects.get(import_href=location_href)
+        #     content["location"] = location
+        # except LocationVO.DoesNotExist:
+        #     return JsonResponse(
+        #         {"message": "Invalid location id"}
+        #         status=400,
+        #     )
+
+        # hat = Hat.objects.create(**content)
+        # return JsonResponse(
+        #     hat,
+        #     encoder=HatListEncoder,
+        #     safe=False,
+        # )
